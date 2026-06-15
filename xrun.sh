cac4a41a5c11e25dd97b4e9fd5cd308be1ab6c07
Song <song@example.com>
2026-06-15 15:03:44 -0500

    2026-06-15 15:03
    Updated

---
+++ xrun.sh
@@ -0,0 +1,4 @@
+git add -f notebooks/DiffInDiffBorusyak.html
+git add -f plots/dynamic_effects_borusyak.pdf
+git add -f plots/dynamic_effects_activity_all.pdf
+git add -f plots/dynamic_effects_agent_cohort_all.pdf
