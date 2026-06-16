77434a33ddf1e2a3bba25ea7f8956e4954a2f56f
Song <song@example.com>
2026-06-15 23:26:49 -0500

    2026-06-15 23:26
    Updated

--- xrun.sh
+++ xrun.sh
@@ -1,3 +1,4 @@
 # Rscript -e "rmarkdown::render('notebooks/DiffInDiffAll.Rmd')"
 # Rscript -e "rmarkdown::render('notebooks/DiffInDiffTWFE.Rmd')"
-Rscript -e "rmarkdown::render('notebooks/DiffInDiffCallaway.Rmd')"
+# Rscript -e "rmarkdown::render('notebooks/DiffInDiffCallaway.Rmd')"
+Rscript -e "rmarkdown::render('notebooks/AnalyzeSonarQubeWarnings.Rmd')"
